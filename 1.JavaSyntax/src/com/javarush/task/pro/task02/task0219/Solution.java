0302df5d909bc80f22d0a4b8263556364391b337
Михаил <mmamchenkov@fil-it.ru>
2022-11-08 17:39:01 +0300

    add solution java syntax from task 0219

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0219/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task02.task0219;
+
+/* 
+Баги робокопа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int id = 1;
+        int age = 2;
+        String documents = "something";
+        String behavior = "anything";
+    }
+}
