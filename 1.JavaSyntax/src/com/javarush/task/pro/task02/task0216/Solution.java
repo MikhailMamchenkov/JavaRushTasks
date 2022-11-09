fb21f5231adaa234b21f0ec6a82c91c87331cda9
Михаил <mmamchenkov@fil-it.ru>
2022-11-09 19:05:00 +0300

    add solution java syntax from task 2116

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0216/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task02.task0216;
+
+/* 
+Генератор позитивных новостей
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String name = "testName";
+        String date = "12.05.2000";
+        String time = "13:22";
+        String noun = "test1";
+        String adjective  = "test2";
+        String verb = "test3";
+
+        System.out.println("Новости:");
+        System.out.println(name + " " + date + " " + time + " изобрел " + adjective + " " + noun + " который " + verb + " террабайты данных в минуту.");
+    }
+}
