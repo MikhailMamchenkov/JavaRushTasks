d243bcb7359776aa12b6268c95ce420dc03d5dab
Михаил <mmamchenkov@fil-it.ru>
2022-11-15 17:13:38 +0300

    add solution java syntax from task 0410

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0410/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.pro.task04.task0410;
+
+import java.util.Scanner;
+
+/* 
+Второе минимальное число из введенных
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Scanner scr = new Scanner(System.in);
+        int min = scr.nextInt();
+        int secondMin = scr.nextInt();
+        if (min > secondMin) {
+            int tmp = secondMin;
+            secondMin = min;
+            min = tmp;
+        }
+        while (scr.hasNextInt()) {
+            int number = scr.nextInt();
+            if (secondMin == min && number > min) {
+                secondMin = number;
+            } else if (number < min) {
+                secondMin = min;
+                min = number;
+            }
+            else if (number > min && number < secondMin) {
+                secondMin = number;
+            }
+        }
+        System.out.println(secondMin);
+    }
+}
