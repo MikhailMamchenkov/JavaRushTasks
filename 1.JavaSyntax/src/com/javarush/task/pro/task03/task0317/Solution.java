c6e127b1ed16770d9b3a557e609c72666f8bcadc
Михаил <mmamchenkov@fil-it.ru>
2022-11-15 14:43:18 +0300

    add solution java syntax from task 0317

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0317/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.pro.task03.task0317;
+
+import java.util.Scanner;
+
+/* 
+Пищевой конвеер
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Scanner scr = new Scanner(System.in);
+        int num1 = scr.nextInt();
+        int num2 = scr.nextInt();
+        int num3 = scr.nextInt();
+        int num4 = scr.nextInt();
+        if (num1 > 0) {
+            System.out.println(num1);
+        }
+        if (num2 > 0) {
+            System.out.println(num2);
+        }
+        if (num3 > 0) {
+            System.out.println(num3);
+        }
+        if (num4 > 0) {
+            System.out.println(num4);
+        }
+    }
+}
