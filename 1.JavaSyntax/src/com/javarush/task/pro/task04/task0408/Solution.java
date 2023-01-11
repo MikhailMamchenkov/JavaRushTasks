bdd64fc8894238114442da044ab93cbebebddd77
Михаил <mmamchenkov@fil-it.ru>
2023-01-11 16:34:02 +0300

    add solution java syntax from task 0408

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0408/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0408/Solution.java
@@ -9,5 +9,14 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int max = Integer.MIN_VALUE;
+        while (scanner.hasNextInt()) {
+            int inputNumber = scanner.nextInt();
+            if (inputNumber % 2 == 0 && inputNumber > max) {
+                max = inputNumber;
+            }
+        }
+        System.out.println(max);
     }
 }
