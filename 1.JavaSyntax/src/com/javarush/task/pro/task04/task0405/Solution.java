0951ccee79b9c865f2c3605c92e594ab5187e97b
Михаил <mmamchenkov@fil-it.ru>
2023-01-11 14:17:31 +0300

    add solution java syntax from task 0421

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0405/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task04.task0405;
+
+/* 
+Незаполненный прямоугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int x = 0;
+        while (x < 10) {
+            int y = 0;
+            while (y < 20) {
+                System.out.print("Б");
+                y++;
+            }
+            System.out.println();
+            x++;
+        }
+
+    }
+}
