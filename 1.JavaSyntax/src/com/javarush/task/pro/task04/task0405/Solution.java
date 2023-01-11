d5c3f145683f84c8a6bca8eeef04b1cac23faa8f
Михаил <mmamchenkov@fil-it.ru>
2023-01-11 16:15:46 +0300

    add solution java syntax from task 0405

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0405/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0405/Solution.java
@@ -7,6 +7,22 @@ package com.javarush.task.pro.task04.task0405;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        int x = 0;
+        while (x < 10) {
+            int y = 0;
+            while (y < 20) {
+                if (x == 0 || x == 9) {
+                    System.out.print("Б");
+                } else if (y == 0 || y == 19) {
+                    System.out.print("Б");
+                } else {
+                    System.out.print(" ");
+                }
+                y++;
+            }
+            System.out.println();
+            x++;
+        }
 
     }
 }
