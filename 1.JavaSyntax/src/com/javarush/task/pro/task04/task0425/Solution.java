4949b263738a0203b9c237961a45e93750a1874d
Михаил <mmamchenkov@fil-it.ru>
2023-01-18 10:30:16 +0300

    add solution java syntax from task 0425

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
@@ -13,13 +13,10 @@ public class Solution {
 
 
     public static void main(String[] args) {
-        //напишите тут ваш код
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                if (i == 8 && j == 3) {
-                    System.out.print(robot);
-                } else if (i == 9) {
-                    System.out.print(bug);
+                if (i == 9) {
+                    System.out.print(death);
                 } else if (j == 3) {
                     System.out.print(hole);
                 } else {
