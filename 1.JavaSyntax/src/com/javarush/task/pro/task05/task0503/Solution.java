77a1f3aecdc7c672f4cf0ec24bb8b8a966e48daf
Михаил <mmamchenkov@fil-it.ru>
2023-01-18 16:43:35 +0300

    add solution java syntax from task 0503

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0503/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0503/Solution.java
@@ -9,27 +9,13 @@ public class Solution {
     public static int[] array = new int[]{-1, 2, 3, -4, -5};
 
     public static void main(String[] args) {
-        //напишите тут ваш код
-        if (array[0] % 2 == 0) {
-            array[0] = -array[0];
+        for (int i = 0; i < array.length ; i++) {
+           if (array[i] % 2 == 0) {
+               //array[i] = - array[i];
+               //array[i] = array[i] * -1;
+               array[i] *= -1;
+           }
+            System.out.println(array[i]);
         }
-        if (array[1] % 2 == 0) {
-            array[1] = -array[1];
-        }
-        if (array[2] % 2 == 0) {
-            array[2] = -array[2];
-        }
-        if (array[3] % 2 == 0) {
-            array[3] = -array[3];
-        }
-        if (array[4] % 2 == 0) {
-            array[4] = -array[4];
-        }
-
-        System.out.println(array[0]);
-        System.out.println(array[1]);
-        System.out.println(array[2]);
-        System.out.println(array[3]);
-        System.out.println(array[4]);
     }
 }
