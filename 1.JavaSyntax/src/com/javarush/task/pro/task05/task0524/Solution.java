3ba5ad1d1b7aefb5cf96bfe207b7fc23295b07fd
Михаил <mmamchenkov@fil-it.ru>
2023-01-25 16:53:18 +0300

    add solution java syntax from task 0524

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0524/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0524/Solution.java
@@ -5,11 +5,10 @@ package com.javarush.task.pro.task05.task0524;
 */
 
 public class Solution {
-    public static int[] coordinates;
+    public static int[] coordinates = new int[200];
 
     public static void main(String[] args) {
-        coordinates = new int [200];
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < coordinates.length / 2; i++) {
             coordinates[i * 2] = i * 10;
             coordinates[i * 2 + 1] = i * 10 + 1;
         }
