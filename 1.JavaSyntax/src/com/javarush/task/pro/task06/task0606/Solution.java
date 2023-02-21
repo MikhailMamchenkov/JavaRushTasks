27edb72ce190331b683b8422c15b956c3dcd05c3
Михаил <mmamchenkov@fil-it.ru>
2023-02-21 17:12:20 +0300

    add solution java syntax from task 0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod() {
+
+    }
+
+    public static void universalMethod(String param) {
+
+    }
+
+    public static void universalMethod(String paramName, String paramValue) {
+
+    }
+
+    public static void universalMethod(int paramName) {
+
+    }
+
+    public static void universalMethod(int paramName, int paramValue) {
+
+    }
+
+    public static void universalMethod(Boolean paramName) {
+
+    }
+
+    public static void universalMethod(Boolean paramName, Boolean paramValue) {
+
+    }
+
+    public static void universalMethod(Double paramName) {
+
+    }
+
+    public static void universalMethod(Double paramName, Double paramValue) {
+
+    }
+
+    public static void universalMethod(Short paramName) {
+
+    }
+}
